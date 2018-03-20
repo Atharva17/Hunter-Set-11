3688a2fa4478da4a7cc7b3999fca8b225f04563c
Harshini A <harshinidurai98@gmail.com>
2018-03-20 16:12:17 +0530

    Update (1^4)+(2^4)etc.c

--- (1^4)+(2^4)etc.c
+++ (1^4)+(2^4)etc.c
@@ -2,7 +2,7 @@
 #include<math.h>
 int main(void) 
 {
-   int power,rev=0,num,count=0,sum=0,t,i,j,a[10];
+   int power,num,t;
    scanf("%d",&num);
    while(num!=0)
    {
@@ -10,6 +10,6 @@ int main(void)
             power=power+pow(t,4);
    	num/=10;
    }
-  printf("sum of series %d",power);
+  printf("%d",power);
 	return 0;
 }
